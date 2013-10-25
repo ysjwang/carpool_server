2de529affec0b7f69d72d4b83412bc7d130c6fe0
Josiah Wang <ysjwang@gmail.com>
2013-10-25 14:47:49 -0400

    Added user_id to contact belongs_to

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: contacts
+#
+#  id         :integer          not null, primary key
+#  first_name :string(255)
+#  last_name  :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#  user_id    :integer
+#
+
 class Contact < ActiveRecord::Base
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: contacts
+#
+#  id         :integer          not null, primary key
+#  first_name :string(255)
+#  last_name  :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#  user_id    :integer
+#
+
 require 'spec_helper'
 
 describe Contact do
